7207e4253e42a8e7557f7ead85381f8949f84f84
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-08-12 16:36:17 +0000

    Implemented simple dummy tree view

--- assignments-view/src/browser/assignments-view-frontend-module.ts
+++ assignments-view/src/browser/assignments-view-frontend-module.ts
@@ -1,16 +1,37 @@
-import { ContainerModule } from 'inversify';
+import { ContainerModule, interfaces } from 'inversify';
 import { AssignmentsViewWidget } from './assignments-view-widget';
 import { AssignmentsViewContribution } from './assignments-view-contribution';
-import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from '@theia/core/lib/browser';
+import {
+    bindViewContribution,
+    createTreeContainer,
+    FrontendApplicationContribution,
+    TreeWidget,
+    WidgetFactory,
+    TreeImpl,
+    Tree
+} from "@theia/core/lib/browser";
+import { AssignmentsTree } from "./assignments-tree";
 
 import '../../src/browser/style/index.css';
 
 export default new ContainerModule(bind => {
     bindViewContribution(bind, AssignmentsViewContribution);
     bind(FrontendApplicationContribution).toService(AssignmentsViewContribution);
-    bind(AssignmentsViewWidget).toSelf();
     bind(WidgetFactory).toDynamicValue(ctx => ({
         id: AssignmentsViewWidget.ID,
-        createWidget: () => ctx.container.get<AssignmentsViewWidget>(AssignmentsViewWidget)
+        createWidget: () => createAssignmentViewWidget(ctx.container)
     })).inSingletonScope();
 });
+
+export function createAssignmentViewWidget(parent: interfaces.Container) : AssignmentsViewWidget {
+    const child = createTreeContainer(parent);
+  
+    child.unbind(TreeImpl);
+    child.bind(AssignmentsTree).toSelf();
+    child.rebind(Tree).toService(AssignmentsTree);
+  
+    child.unbind(TreeWidget);
+    child.bind(AssignmentsViewWidget).toSelf();
+  
+    return child.get(AssignmentsViewWidget);   
+}
--- assignments-view/src/browser/assignments-view-widget.tsx
+++ assignments-view/src/browser/assignments-view-widget.tsx
@@ -1,39 +1,103 @@
-import * as React from 'react';
-import { injectable, postConstruct, inject } from 'inversify';
-import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
-import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
-import { MessageService } from '@theia/core';
+import { injectable, inject } from 'inversify';
+import {
+    ContextMenuRenderer,
+    TreeModel,
+    TreeProps,
+    TreeWidget,
+    TreeNode,
+    ExpandableTreeNode
+} from "@theia/core/lib/browser";
+import { FamilyRootNode, MemberNode } from "./assignments-tree";
 
 @injectable()
-export class AssignmentsViewWidget extends ReactWidget {
-
+export class AssignmentsViewWidget extends TreeWidget {
     static readonly ID = 'assignments-view:widget';
     static readonly LABEL = 'Assignments View';
 
-    @inject(MessageService)
-    protected readonly messageService!: MessageService;
+    // @postConstruct()
+    // protected async init(): Promise<void> {
+    //     this.id = AssignmentsViewWidget.ID;
+    //     this.title.label = AssignmentsViewWidget.LABEL;
+    //     this.title.caption = AssignmentsViewWidget.LABEL;
+    //     this.title.closable = true;
+    //     this.title.iconClass = 'fa fa-window-maximize'; // example widget icon.
+    //     this.update();
+    // }
 
-    @postConstruct()
-    protected async init(): Promise < void> {
+    constructor(
+        @inject(TreeProps) readonly props: TreeProps,
+        @inject(TreeModel) readonly model: TreeModel,
+        @inject(ContextMenuRenderer) contextMenuRenderer: ContextMenuRenderer
+    ) {
+        super(props, model, contextMenuRenderer);
+
         this.id = AssignmentsViewWidget.ID;
         this.title.label = AssignmentsViewWidget.LABEL;
         this.title.caption = AssignmentsViewWidget.LABEL;
         this.title.closable = true;
         this.title.iconClass = 'fa fa-window-maximize'; // example widget icon.
-        this.update();
+
+        const family: Family = {
+            name: "Vestrit",
+            members: [
+                {
+                    firstName: "Ephron",
+                    nickName: "Ephy",
+                    children: [
+                        {
+                            firstName: "Keffria",
+                            nickName: "Keff",
+                            children: [
+                                {
+                                    firstName: "Wintrow",
+                                    nickName: "Win"
+                                },
+                                {
+                                    firstName: "Malta",
+                                    nickName: "Ederling Queen",
+                                    children: [
+                                        {
+                                            firstName: "Ephron Bendir",
+                                            nickName: "Ben"
+                                        }
+                                    ]
+                                },
+                                {
+                                    firstName: "Selden",
+                                    nickName: "Ederling Prince"
+                                }
+                            ]
+                        },
+                        {
+                            firstName: "Althea",
+                            nickName: "Alth"
+                        }
+                    ]
+                }
+            ]
+        };
+
+        const root: FamilyRootNode = {
+            id: "assignments-root",
+            name: "assignments-root",
+            visible: false,
+            parent: undefined,
+            children: [],
+            family
+        };
+
+        this.model.root = root;
+        // Maybe????
+        // this.update();
     }
 
-    protected render(): React.ReactNode {
-        const header = `This is a sample widget which simply calls the messageService
-        in order to display an info message to end users.`;
-        return <div id='widget-container'>
-            <AlertMessage type='INFO' header={header} />
-            <button className='theia-button secondary' title='Display Message' onClick={_a => this.displayMessage()}>Display Message</button>
-        </div>
-    }
+    protected isExpandable(node: TreeNode): node is ExpandableTreeNode {
+        if (FamilyRootNode.is(node)) return true;
 
-    protected displayMessage(): void {
-        this.messageService.info('Congratulations: AssignmentsView Widget Successfully Created!');
+        if (MemberNode.is(node) && node.member.children)
+            return node.member.children.length > 0;
+
+        return false;
     }
 
 }
