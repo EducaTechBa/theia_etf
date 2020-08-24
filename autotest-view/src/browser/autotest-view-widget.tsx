ed2ed1ce6752c86879446d5fa871a6b426fcbac7
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-08-24 16:01:50 +0000

    Implemented simple autotesting request

--- autotest-view/src/browser/autotest-view-widget.tsx
+++ autotest-view/src/browser/autotest-view-widget.tsx
@@ -5,6 +5,7 @@ import { MessageService } from '@theia/core';
 import { EditorManager } from '@theia/editor/lib/browser';
 import { FileSystem } from '@theia/filesystem/lib/common';
 import { Autotester } from './autotester';
+import URI from '@theia/core/lib/common/uri';
 
 @injectable()
 export class AutotestViewWidget extends ReactWidget {
@@ -40,25 +41,49 @@ export class AutotestViewWidget extends ReactWidget {
         </div>
     }
 
-    private handleRunTests() {
-        const editor = this.editorManager.currentEditor;
-        if (editor) {
-            const uri = editor.getResourceUri();
-            if (!uri) {
-                this.messageService.info("File not saved!");
-                return;
-            }
-            const autotestFileURI = `${uri.parent}/.autotest2`;
-            this.fileSystem.resolveContent(autotestFileURI)
-                .then(file => {
-                    const autotest = JSON.parse(file.content);
-                    this.messageService.info(`Autotest ID: ${autotest.id}`);
-                    this.autotester.setProgramFile("program id", "main.c", "");
-                })
-                .catch(err => console.log(err));
-        } else {
-            this.messageService.info("No file currently opened!");
+    // Disable test button while testing in progress???
+    private async handleRunTests() {
+        const currentFile = this.getCurrentEditorFile();
+        if (!currentFile) {
+            this.messageService.info("Please open a source file to test!");
+            return;
         }
+
+        // Kako da znam koji je file zapravo source file????
+        const sourceFilename = 'main.c';
+        const dir = currentFile.parent.toString();
+
+        const autotestContent = await this.loadFileContent(dir , '.autotest2');
+        const sourceContent = await this.loadFileContent(dir, sourceFilename);
+
+        const autotest = JSON.parse(autotestContent);
+        const taskID = await this.autotester.setTask(autotest);
+        console.log(`Task ID: ${taskID}`);
+        // Check if a program exists first, otherwise create a new one...
+        const programID = await this.autotester.setProgram(taskID);
+        console.log(`Program ID: ${programID}`);
+        await this.autotester.setProgramFile(programID, sourceFilename, sourceContent);
+        console.log("Source file is set...");
+    }
+
+    private getCurrentEditorFile(): URI | null {
+        const editor = this.editorManager.currentEditor;
+        if (!editor) {
+            return null;
+        }
+
+        const uri = editor.getResourceUri();
+        if (!uri) {
+            return null;
+        }
+
+        return uri;
+    }
+
+    private async loadFileContent(dir: string, filename: string): Promise<string> {
+        const uri = `${dir}/${filename}`;
+        const file = await this.fileSystem.resolveContent(uri);
+        return file.content;
     }
 
 }
--- autotest-view/src/browser/autotester.ts
+++ autotest-view/src/browser/autotester.ts
@@ -1,44 +1,110 @@
 import { injectable } from 'inversify';
 import * as JSZip from 'jszip';
+import { StatefulWidget } from '@theia/core/lib/browser';
+
+interface AutotesterState {
+    programIDs: any
+};
+
+namespace AutotesterState {
+    export function is(obj: object): obj is AutotesterState {
+        return !!obj && "programIDs" in obj;
+    }
+}
 
 @injectable()
-export class Autotester {
+export class Autotester implements StatefulWidget {
 
-    public async setTask(): Promise<string> {
-        return "";
+    private state: AutotesterState;
+
+    storeState(): object {
+        return this.state;
     }
 
+    restoreState(oldState: object): void {
+        this.state = AutotesterState.is(oldState) ? oldState as AutotesterState : {
+            programIDs: {
+                '4093': '32777'
+            }
+        };
+    }
+
+    private makeURL(action: string, queryParams: string) {
+        return `/autotester/server/push.php?action=${action}&${queryParams}`;
+    }
+
+    public async setTask(autotest: any): Promise<string> {
+        const autotestQuery = encodeURIComponent(JSON.stringify(autotest));
+        const url = this.makeURL('setTask', `task=${autotestQuery}`);
+        const res = await fetch(url, {
+            method: 'POST'
+        });
+        const data = await res.json();
+
+        console.log(JSON.stringify(data));
+
+        return data.data;
+    }
+
+    public async setProgram(taskID: string): Promise<string> {
+        console.log(JSON.stringify(this.state));
+
+        const program = { task: taskID, name: '' };
+        const programQuery = encodeURIComponent(JSON.stringify(program));
+        const url = this.makeURL('setProgram', `program=${programQuery}`);
+
+        const res = await fetch(url, {
+            method: 'POST'
+        });
+
+        const data = await res.json();
+        return data.data;
+    }
+
+    // TODO: Remove. This method will not be used...
     public async getPrograms(taskID: string): Promise<any> {
-        const res = await fetch(`/autotester/server/push.php?action=listPrograms&task=${taskID}`);
+        const url = this.makeURL('listPrograms', `task=${taskID}`)
+        const res = await fetch(url);
         const data = await res.json();
 
         return data.data;
     }
 
     public async setProgramFile(programID: string, filename: string, filecontent: string) {
-        // read file contetn
-        // cosnt formData = new FormData()
-        // formData.append(fileContent);
-        // fetch('', { data: formData })
-
-        // program id: 32777
-        const program_id = 32777;
-        const url = `/autotester/server/push.php?action=setProgramFile&id=${program_id}`;
+        const url = this.makeURL('setProgramFile', `id=${programID}`);
 
         const zip = new JSZip();
         zip.file(filename, filecontent);
-        zip.generateAsync({ type: 'blob' })
-            .then(content => {
-                const formData = new FormData();
-                formData.append('program', content);
-                fetch(url, {
-                    method: 'POST',
-                    body: formData
-                }).then(res => res.text())
-                .then(data => console.log(data))
-                .catch(err => console.log(err));
-            })
-            .catch(err => console.log(err));
+        const content = await zip.generateAsync({ type: 'blob' });
+
+        const formData = new FormData();
+        formData.append('program', content);
+        await fetch(url, {
+            method: 'POST',
+            body: formData
+        });
+    }
+
+    public async getResults(programID: string) {
+        const url = this.makeURL('getResult', `id=${programID}`);
+        const res = await fetch(url);
+        const data = await res.text();
+
+        console.log(data);
+    }
+
+    public async writeAutotestResultsFile() {
+        // TODO: Implement
+    }
+
+    public async retest(programID: string) {
+        const url = this.makeURL('retest', `id=${programID}`);
+        const res = await fetch(url, {
+            method: 'POST'
+        });
+        const data = await res.text();
+
+        console.log(data);
     }
 
 }
