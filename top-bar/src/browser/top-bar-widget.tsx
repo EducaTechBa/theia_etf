e2a5c34f2964a005d9f868e16e8a536f3b9815c0
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-09-09 20:12:22 +0000

    Implemented run button

--- top-bar/src/browser/top-bar-widget.tsx
+++ top-bar/src/browser/top-bar-widget.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { injectable, postConstruct, inject } from 'inversify';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
 import { MessageService } from '@theia/core';
+import { TaskService } from '@theia/task/lib/browser';
+import { EditorManager, EditorWidget } from '@theia/editor/lib/browser';
 
 interface TopBarButtonProps {
     text: string;
@@ -17,8 +19,16 @@ export class TopBarWidget extends ReactWidget {
     static readonly ID = 'top-bar:widget';
     static readonly LABEL = 'TopBar';
 
+    private editorFileURI: string | undefined;
+
     @inject(MessageService)
-    protected readonly messageService!: MessageService;
+    protected readonly messageService: MessageService;
+
+    @inject(TaskService)
+    protected readonly taskService: TaskService;
+
+    @inject(EditorManager)
+    protected readonly editorManager!: EditorManager;
 
     @postConstruct()
     protected async init(): Promise<void> {
@@ -27,7 +37,17 @@ export class TopBarWidget extends ReactWidget {
         this.title.caption = TopBarWidget.LABEL;
         this.title.closable = true;
         this.title.iconClass = 'fa fa-window-maximize';
+
+        this.editorManager.onCreated(editorWidget => this.handleEditorSwitch(editorWidget));
+        this.editorManager.onActiveEditorChanged(editorWidget => this.handleEditorSwitch(editorWidget));
+
         this.update();
+    }
+
+    private handleEditorSwitch(editorWidget: EditorWidget | undefined) {
+        if (editorWidget !== undefined) {
+            this.editorFileURI = editorWidget?.getResourceUri()?.toString();
+        }
     }
 
     protected render(): React.ReactNode {
@@ -55,8 +75,14 @@ export class TopBarWidget extends ReactWidget {
             text: 'Run',
             tooltip: 'Run current program',
             iconClass: 'fa fa-play',
-            onClick: () => this.messageService.info("Run button pressed...")
+            onClick: () => this.handleRunButtonClick()
         });
+    }
+
+    private async handleRunButtonClick() {
+        if (this.editorFileURI) {
+            this.taskService.runTaskByLabel(0, 'Build and Run');
+        }
     }
 
     private renderSubmitButton(): React.ReactNode {
