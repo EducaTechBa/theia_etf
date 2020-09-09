f1d199feac31ae04febba6e0887293ac585709c2
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-09-09 10:59:37 +0000

    Implemented easy button adding

--- top-bar/src/browser/top-bar-widget.tsx
+++ top-bar/src/browser/top-bar-widget.tsx
@@ -3,6 +3,14 @@ import { injectable, postConstruct, inject } from 'inversify';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
 import { MessageService } from '@theia/core';
 
+interface TopBarButtonProps {
+    text: string;
+    tooltip?: string;
+    classNames?: string;
+    iconClass?: string;
+    onClick: Function
+}
+
 @injectable()
 export class TopBarWidget extends ReactWidget {
 
@@ -13,7 +21,7 @@ export class TopBarWidget extends ReactWidget {
     protected readonly messageService!: MessageService;
 
     @postConstruct()
-    protected async init(): Promise < void> {
+    protected async init(): Promise<void> {
         this.id = TopBarWidget.ID;
         this.title.label = TopBarWidget.LABEL;
         this.title.caption = TopBarWidget.LABEL;
@@ -23,13 +31,49 @@ export class TopBarWidget extends ReactWidget {
     }
 
     protected render(): React.ReactNode {
-        return <div id='widget-container'>
-            <button className='theia-button secondary' title='Submit Homework' onClick={_a => this.displayMessage()}>Submit</button>
+        return <div id='top-bar-container'>
+            <span>
+                {this.renderRunButton()}
+                {this.renderSubmitButton()}
+            </span>
+            {this.renderLogoutButton()}
         </div>
     }
 
-    protected displayMessage(): void {
-        this.messageService.info('Congratulations: TopBar Widget Successfully Created!');
+    protected renderButton(props: TopBarButtonProps): React.ReactNode {
+        return <button
+            className={`theia-button secondary ${props.classNames ?? ''}`}
+            title={props.tooltip ?? props.text}
+            onClick={event => props.onClick(event)}>
+            {props.iconClass && <i className={`button-icon ${props.iconClass}`} aria-hidden="true"></i>}
+            {props.text}
+        </button>
+    }
+
+    private renderRunButton(): React.ReactNode {
+        return this.renderButton({
+            text: 'Run',
+            tooltip: 'Run current program',
+            iconClass: 'fa fa-play',
+            onClick: () => this.messageService.info("Run button pressed...")
+        });
+    }
+
+    private renderSubmitButton(): React.ReactNode {
+        return this.renderButton({
+            text: 'Submit',
+            tooltip: 'Submit current homework program',
+            iconClass: 'fa fa-envelope',
+            onClick: () => this.messageService.info("Submit button pressed...")
+        });
+    }
+
+    private renderLogoutButton(): React.ReactNode {
+        return this.renderButton({
+            text: 'Log Out',
+            iconClass: 'fa fa-sign-out',
+            onClick: () => this.messageService.info("Logging out...")
+        });
     }
 
 }
