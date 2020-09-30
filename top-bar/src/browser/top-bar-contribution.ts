db92ced002b0c407c9aca150118877529ee93bcb
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-09-30 07:15:32 +0000

    Removed terminal closing on debug session end

--- top-bar/src/browser/top-bar-contribution.ts
+++ top-bar/src/browser/top-bar-contribution.ts
@@ -1,4 +1,4 @@
-import { injectable, inject, postConstruct } from 'inversify';
+import { injectable, inject } from 'inversify';
 import { MenuModelRegistry } from '@theia/core';
 import { TopBarWidget } from './top-bar-widget';
 import { AbstractViewContribution } from '@theia/core/lib/browser';
@@ -8,8 +8,6 @@ import { WorkspaceService } from '@theia/workspace/lib/browser';
 import { FrontendApplication, FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { MaybePromise } from '@theia/core/lib/common/types';
 import { TerminalMenus, TerminalCommands } from '@theia/terminal/lib/browser/terminal-frontend-contribution';
-import { DebugSessionManager } from '@theia/debug/lib/browser/debug-session-manager';
-import { TerminalService } from '@theia/terminal/lib/browser/base/terminal-service';
 
 export const TopBarCommand: Command = { id: 'top-bar:command' };
 
@@ -22,30 +20,12 @@ export class TopBarContribution extends AbstractViewContribution<TopBarWidget> i
     @inject(WorkspaceService)
     protected readonly workspaceService: WorkspaceService;
 
-    @inject(DebugSessionManager)
-    protected readonly debugSessionManager: DebugSessionManager;
-
-    @inject(TerminalService)
-    protected readonly terminalService: TerminalService;
-
     constructor() {
         super({
             widgetId: TopBarWidget.ID,
             widgetName: TopBarWidget.LABEL,
             defaultWidgetOptions: { area: 'top' },
             toggleCommandId: TopBarCommand.id
-        });
-    }
-
-    @postConstruct()
-    // @ts-ignore
-    private init() {
-        this.debugSessionManager.onDidDestroyDebugSession(debugSession => {
-            this.terminalService.all.forEach(terminalWidget => {
-                if (terminalWidget.title.label.includes('cppdbg')) {
-                    terminalWidget.close();
-                }
-            })
         });
     }
 
