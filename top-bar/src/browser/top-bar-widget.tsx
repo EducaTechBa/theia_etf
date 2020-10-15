82ebd2c6400f817a9bfae1c09b0e0235e41bba4b
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-10-15 06:02:30 +0000

    Added pinging for session maintaining

--- top-bar/src/browser/top-bar-widget.tsx
+++ top-bar/src/browser/top-bar-widget.tsx
@@ -47,6 +47,9 @@ export class TopBarWidget extends ReactWidget {
         this.editorManager.onCreated(editorWidget => this.handleEditorSwitch(editorWidget));
         this.editorManager.onActiveEditorChanged(editorWidget => this.handleEditorSwitch(editorWidget));
 
+
+        this.ping();
+
         this.update();
     }
 
@@ -129,6 +132,22 @@ export class TopBarWidget extends ReactWidget {
 
     private async handleLogoutButtonClick() {
         window.location.href = '/index.php?logout';
+    }
+
+    private async ping() {
+        const res = await fetch('/zamger/ping.php');
+        const data = await res.text();
+
+        console.log(data);
+
+        this.delay(60000).then(() => {
+            console.log("60000 ms later");
+            this.ping();
+        });
+    }
+
+    private delay(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
 }
