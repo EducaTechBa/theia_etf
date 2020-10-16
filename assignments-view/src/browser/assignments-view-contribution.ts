4fc8e136ba8731ff96531c44b639e8024bb977ea
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-10-16 07:53:44 +0000

    Automatically open Assignments View

--- assignments-view/src/browser/assignments-view-contribution.ts
+++ assignments-view/src/browser/assignments-view-contribution.ts
@@ -1,13 +1,23 @@
-import { injectable } from 'inversify';
+import { injectable, inject } from 'inversify';
 import { MenuModelRegistry } from '@theia/core';
 import { AssignmentsViewWidget } from './assignments-view-widget';
-import { AbstractViewContribution } from '@theia/core/lib/browser';
+import { AbstractViewContribution, FrontendApplicationContribution, FrontendApplication } from '@theia/core/lib/browser';
 import { Command, CommandRegistry } from '@theia/core/lib/common/command';
+import { MaybePromise } from '@theia/core/lib/common/types';
+import { FrontendApplicationStateService } from '@theia/core/lib/browser/frontend-application-state';
+import { WorkspaceService } from '@theia/workspace/lib/browser';
+
 
 export const AssignmentsViewCommand: Command = { id: 'assignments-view:command' };
 
 @injectable()
-export class AssignmentsViewContribution extends AbstractViewContribution<AssignmentsViewWidget> {
+export class AssignmentsViewContribution extends AbstractViewContribution<AssignmentsViewWidget> implements FrontendApplicationContribution {
+
+    @inject(FrontendApplicationStateService)
+    protected readonly stateService: FrontendApplicationStateService;
+
+    @inject(WorkspaceService)
+    protected readonly workspaceService: WorkspaceService;
 
     constructor() {
         super({
@@ -26,5 +36,13 @@ export class AssignmentsViewContribution extends AbstractViewContribution<Assign
 
     registerMenus(menus: MenuModelRegistry): void {
         super.registerMenus(menus);
+    }
+
+    onStart(app: FrontendApplication): MaybePromise<void> {
+        if (this.workspaceService.opened) {
+            this.stateService.reachedState('ready').then(
+                () => this.openView({ activate: true, reveal: true })
+            );
+        }
     }
 }
