68e3ff194906df4f2c270e4a0c3a5d8698825734
mmesihovic <mmesihovic1@etf.unsa.ba>
2020-10-18 21:01:47 +0000

    Added automatically opening game view widget upon theia start

--- uup-game-view/src/browser/uup-game-view-contribution.ts
+++ uup-game-view/src/browser/uup-game-view-contribution.ts
@@ -1,22 +1,23 @@
-import { injectable } from 'inversify';
+import { injectable, inject } from 'inversify';
 import { MenuModelRegistry } from '@theia/core';
 import { UupGameViewWidget } from './uup-game-view-widget';
-import { AbstractViewContribution } from '@theia/core/lib/browser';
+import { AbstractViewContribution , FrontendApplicationContribution, FrontendApplication} from '@theia/core/lib/browser';
 import { Command, CommandRegistry } from '@theia/core/lib/common/command';
+import { MaybePromise } from '@theia/core/lib/common/types';
+import { FrontendApplicationStateService } from '@theia/core/lib/browser/frontend-application-state';
+import { WorkspaceService } from '@theia/workspace/lib/browser';
 
 export const UupGameViewCommand: Command = { id: 'uup-game-view:command' };
 
 @injectable()
-export class UupGameViewContribution extends AbstractViewContribution<UupGameViewWidget> {
+export class UupGameViewContribution extends AbstractViewContribution<UupGameViewWidget> implements FrontendApplicationContribution {
 
-    /**
-     * `AbstractViewContribution` handles the creation and registering
-     *  of the widget including commands, menus, and keybindings.
-     * 
-     * We can pass `defaultWidgetOptions` which define widget properties such as 
-     * its location `area` (`main`, `left`, `right`, `bottom`), `mode`, and `ref`.
-     * 
-     */
+    @inject(FrontendApplicationStateService)
+    protected readonly stateService: FrontendApplicationStateService;
+
+    @inject(WorkspaceService)
+    protected readonly workspaceService: WorkspaceService;
+
     constructor() {
         super({
             widgetId: UupGameViewWidget.ID,
@@ -26,45 +27,21 @@ export class UupGameViewContribution extends AbstractViewContribution<UupGameVie
         });
     }
 
-    /**
-     * Example command registration to open the widget from the menu, and quick-open.
-     * For a simpler use case, it is possible to simply call:
-     ```ts
-        super.registerCommands(commands)
-     ```
-     *
-     * For more flexibility, we can pass `OpenViewArguments` which define 
-     * options on how to handle opening the widget:
-     * 
-     ```ts
-        toggle?: boolean
-        activate?: boolean;
-        reveal?: boolean;
-     ```
-     *
-     * @param commands
-     */
     registerCommands(commands: CommandRegistry): void {
         commands.registerCommand(UupGameViewCommand, {
             execute: () => super.openView({ activate: false, reveal: true })
         });
     }
 
-    /**
-     * Example menu registration to contribute a menu item used to open the widget.
-     * Default location when extending the `AbstractViewContribution` is the `View` main-menu item.
-     * 
-     * We can however define new menu path locations in the following way:
-     ```ts
-        menus.registerMenuAction(CommonMenus.HELP, {
-            commandId: 'id',
-            label: 'label'
-        });
-     ```
-     * 
-     * @param menus
-     */
     registerMenus(menus: MenuModelRegistry): void {
         super.registerMenus(menus);
+    }
+
+    onStart(app: FrontendApplication): MaybePromise<void> {
+        if (this.workspaceService.opened) {
+            this.stateService.reachedState('ready').then(
+                () => this.openView({ activate: false, reveal: false })
+            );
+        }
     }
 }
