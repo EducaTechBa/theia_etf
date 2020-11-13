bf7cde6f397e6a3fc1750c54a81bd5d2ad079806
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-11-13 19:22:10 +0100

    Hotfix for homework submit

--- top-bar/src/browser/homework-submit.ts
+++ top-bar/src/browser/homework-submit.ts
@@ -1,12 +1,16 @@
 import { injectable, inject } from 'inversify';
 import { WorkspaceService } from '@theia/workspace/lib/browser';
 import { FileService } from '@theia/filesystem/lib/browser/file-service';
+import { EditorManager, EditorWidget } from '@theia/editor/lib/browser';
 import URI from '@theia/core/lib/common/uri';
 
 @injectable()
 export class HomeworkSubmit {
 
     private static readonly HOMEWORK_FILE_NAME = '.zadaca';
+
+    @inject(EditorManager)
+    protected readonly editorManager!: EditorManager;
 
     @inject(WorkspaceService)
     protected readonly workspaceService: WorkspaceService;
@@ -18,14 +22,23 @@ export class HomeworkSubmit {
         if(!this.isHomeworkAssignment(dirURI)) {
             return;
         }
-
+        
         const homeworkFilePath = `${dirURI}/${HomeworkSubmit.HOMEWORK_FILE_NAME}`;
         const homeworkFile = await this.fileService.read(new URI(homeworkFilePath));
         const homework = JSON.parse(homeworkFile.value);
 
+        const editorWidget = this.editorManager.currentEditor;
+        let homeworkContentFilePath = `${dirURI}/`;
+        if (editorWidget) {
+            const names = editorWidget.getResourceUri()?.toString().split('/');
+            const index = names?.length? names.length-1:-1;
+            if(index!==-1 && names) {
+                homeworkContentFilePath+=names[index];
+            }
+        }
         // filename query parameter is used by the service to determine the 
         // file directory... the homeworkFilePath is sufficient...
-        const url = `/zamger/slanje_zadace.php?zadaca=${homework.id}&zadatak=${homework.zadatak}&filename=${homeworkFilePath}`;
+        const url = `/zamger/slanje_zadace.php?zadaca=${homework.id}&zadatak=${homework.zadatak}&filename=${homeworkContentFilePath}`;
 
         await fetch(url, {
             method: 'GET',
